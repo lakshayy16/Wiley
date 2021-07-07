5fd490cddb34722990196fbf9c8d66e7ecc44033
lakshayy16 <lakshay.agar@gmail.com>
2021-07-07 15:35:57 +0530

    mp3

--- src/jpmorgan/banking/withdraw/MiniProjectBank.java
+++ src/jpmorgan/banking/withdraw/MiniProjectBank.java
@@ -1,8 +1,27 @@
 package jpmorgan.banking.withdraw;
-
+/*Build an application that constitutes for a banking platform, it should handle the account transacs,
+ * 	for credit and debit as per the banking systems , we should 
+ * 	have the account details tracked as well.
+ * 	Topics to be used: Java = ( Streams , Lambdas / Oops / 
+ * 								List Collection)
+ *		
+ * Menu Driven Approach
+ * Check if old user / new user 
+ * If new user - validation of Pin of the bank account
+ * Opening message - with the options for the actions that can be performed by the user
+ * according to the option opted for E.g Credit/Debit do accordingly 
+ * if debit needs to be performed , have to check if the amount debitted is present in the account 
+ * 	- The account in which the amount will be transferred is a valid account 
+ * 	- Transaction/authentication password confirmation 
+ *  - once the transaction takes place , both the parties query is resolved then only commit or else 
+ *  	rollback takes place in case of error.
+ *  - a confirmation message with the menu being shown again for any other action to be done .
+ * 
+ * 
+ */
 public class MiniProjectBank {
 	public static void main()
 	{
-		System.out.println("Hello testing");
+		// 
 	}
 }

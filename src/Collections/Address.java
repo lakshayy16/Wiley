6fd9d21589fc1d3ae36fc2a1f66070815b97d017
lakshayy16 <lakshay.agar@gmail.com>
2021-07-09 04:23:57 +0530

    learning

--- src/Collections/Address.java
+++ src/Collections/Address.java
@@ -8,26 +8,71 @@ import java.util.TreeSet;
 public class Address {
 	public static void main(String args[])
 	{
-//		User u1 = new User(4,"Vanessa");
-//		User u2 = new User(3,"Albert");
-//		User u3 = new User(1,"Tom");
+		List<Add> Address = new ArrayList<>();
+		Address.add(new Add("BLR",560001));
+		Address.add(new Add("DEL",110001));
+		Address.add(new Add("GN",201305));
+		
+		List<User> user = new ArrayList<>();
+		user.add(new User(2, "User2", Arrays.asList(Address.get(1), Address.get(0))));
+		user.add(new User(1, "User1", Arrays.asList(Address.get(2), Address.get(0))));
+		user.add(new User(3, "User3", Arrays.asList(Address.get(2), Address.get(0))));
+		
+		System.out.println("[");
+		Address.forEach(i->{
+			List<User> temp = new ArrayList<>(user.stream().filter(a-> a.isCityExist(i.getCity())).toList());
+			if(temp.size()>1) {
+				System.out.print(" address { city: " + i.getCity()+
+						" , zip:"+i.getZip()+
+						" ,users: [");
+				temp.stream().forEach(System.out::print);
+				System.out.println("] },");
+			}
+		});
+		System.out.println("]");
+
 	}
 }
-class User{
-	private int id;
-	private String name;
-	TreeSet<Object> Address ;
-	
-	public User(int id, String name,TreeSet add) {
+
+class Add{
+	String city;
+	int zip;
+	public Add(String city,int zip) {
 		super();
-		this.id = id;
-		this.name = name;
-		this.Address= new TreeSet<Object>();
-		this.Address=add;
+		this.city=city;
+		this.zip=zip;
 	}
 	@Override
 	public String toString() {
-		return id + " " + name ;
+		return("{ city: '" + city + "' , zip: "+zip+" }");
+	}
+	public String getCity() {
+		return city;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setCity(String city) {
+		this.city=city;
+	}
+	public void setZip(int zip) {
+		this.zip=zip;
+	}
+}
+class User{
+	int id;
+	String name;
+	List<Add> address ;
+	
+	public User(int id, String name,List<Add> address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address=address;
+	}
+	@Override
+	public String toString() {
+		return ("User{ id:"+ id+","+" name: '"+name+"'}, ");
 	}
 	public int getId() {
 		return id;
@@ -40,6 +85,15 @@ class User{
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public List<Add> getAddress(){
+		return address;
+	}
+	public void setAddress(List<Add> address) {
+		this.address = address;
+	}
+	public boolean isCityExist(String city) {
+		return address.stream().anyMatch(a->a.getCity().equals(city));
 	}
 }
 
